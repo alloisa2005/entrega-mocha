19264a43b77019d444e12d23a53c7137125ba371
Anthony Allois <anthonyallois@gmail.com>
2023-02-02 09:45:49 -0300

    feat: Creando rutas

---
+++ app.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+let PORT = process.env.PORT || 8080;
+
+const productRouter = require('./routes/product.routes');
+const userRouter = require('./routes/user.routes');
+
+//////// Conexión MongoDB ////////
+require('./database');
+
+
+////////////// Rutas //////////////  
+app.use('/api/productos', productRouter);
+app.use('/api/users', userRouter);
+
+
+app.listen(PORT, () => console.log(`Server Up on Port ${PORT}`));
---
+++ database.js
@@ -0,0 +1,11 @@
+
+const mongoose = require('mongoose');
+
+
+const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@clusteranthony.rf4uk66.mongodb.net/${process.env.MONGODB_DB}?retryWrites=true&w=majority`;
+
+mongoose.set('strictQuery', true);
+
+mongoose.connect(uri)
+  .then( () => console.log(`DB: ${process.env.MONGODB_DB} lista para usar`))
+  .catch( err => console.log(err))
